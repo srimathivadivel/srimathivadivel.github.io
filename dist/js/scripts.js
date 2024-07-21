4c287bd932daac5c93c9aa00a6accf7584e1c3d6
srimathivadivel <srimathi.vadivel@gmail.com>
2024-07-21 17:56:26 -0400

    minor design changes

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -49,75 +49,6 @@ anime.timeline({loop: true})
     delay: 1000
   });
 
-/* bouncing dot h1 */
-
-const bouncingDot = document.querySelector('.bouncing-dot');
-
-const bouncingPatterns = [
-  {
-    startTime: 0,
-    duration: 1000,
-    endY: '20%',
-    endScale: 1.2
-  },
-  {
-    startTime: 1000,
-    duration: 800,
-    endY: '40%',
-    endScale: 1
-  },
-  {
-    startTime: 2000,
-    duration: 800,
-    endY: '60%',
-    endScale: 1.3
-  },
-  {
-    startTime: 3000,
-    duration: 800,
-    endY: '80%',
-    endScale: 1.2
-  },
-  {
-    startTime: 4000,
-    duration: 800,
-    endY: '10%',
-    endScale: 1.3
-  }
-];
-
-function generateBouncingAnimation() {
-  let currentPatternIndex = 0;
-  let startTime = null;
-
-  function updateBouncingDot(time) {
-    if (startTime === null) {
-      startTime = time;
-    }
-
-    const elapsedTime = time - startTime;
-    const currentPattern = bouncingPatterns[currentPatternIndex];
-
-    if (elapsedTime < currentPattern.duration) {
-      const progress = elapsedTime / currentPattern.duration;
-      const translateY = `calc(${currentPattern.endY} * ${progress})`;
-      const scale = 1 + (currentPattern.endScale - 1) * progress;
-
-      bouncingDot.style.transform = `translate(-50%, ${translateY}) scale(${scale})`;
-    } else {
-      currentPatternIndex++;
-
-      if (currentPatternIndex >= bouncingPatterns.length) {
-        currentPatternIndex = 0;
-      }
-
-      startTime = null;
-    }
-    requestAnimationFrame(updateBouncingDot);
-  }
-  requestAnimationFrame(updateBouncingDot);
-}
-generateBouncingAnimation();
 
 /* for the project cards */
 
@@ -137,4 +68,51 @@ projectModals.forEach((modal) => {
     }
   });
 });
+
+window.addEventListener('DOMContentLoaded', event => {
+
+  // Navbar shrink function
+  var navbarShrink = function () {
+      const navbarCollapsible = document.body.querySelector('#mainNav');
+      if (!navbarCollapsible) {
+          return;
+      }
+      if (window.scrollY === 0) {
+          navbarCollapsible.classList.remove('navbar-shrink')
+      } else {
+          navbarCollapsible.classList.add('navbar-shrink')
+      }
+
+  };
+
+  // Shrink the navbar 
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener('scroll', navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector('#mainNav');
+  if (mainNav) {
+      new bootstrap.ScrollSpy(document.body, {
+          target: '#mainNav',
+          rootMargin: '0px 0px -40%',
+      });
+  };
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+      document.querySelectorAll('#navbarResponsive .nav-link')
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+      responsiveNavItem.addEventListener('click', () => {
+          if (window.getComputedStyle(navbarToggler).display !== 'none') {
+              navbarToggler.click();
+          }
+      });
+  });
+
+});
+
 
